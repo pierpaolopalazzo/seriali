bffe30169128903825690ba94aa64e9ccf843e7d
mrutigliano <maurorutigliano@gmail.com>
2016-05-24 11:30:20 +0200

    aaa

--- SERIALI-SG/Form_00_HOME.Designer.vb
+++ SERIALI-SG/Form_00_HOME.Designer.vb
@@ -35,6 +35,7 @@ Partial Class Form_00_HOME
         Me.btnSetup = New System.Windows.Forms.Button()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.LabelSetup = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBoxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -150,11 +151,21 @@ Partial Class Form_00_HOME
         Me.LabelSetup.TabIndex = 9
         Me.LabelSetup.Text = "Setup"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(232, 548)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Label1"
+        '
         'Form_00_HOME
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(395, 674)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnSetup)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.LabelSetup)
@@ -186,4 +197,5 @@ Partial Class Form_00_HOME
     Friend WithEvents btnSetup As Button
     Friend WithEvents Label5 As Label
     Friend WithEvents LabelSetup As Label
+    Friend WithEvents Label1 As Label
 End Class
